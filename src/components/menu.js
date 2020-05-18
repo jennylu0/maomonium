eb32c4f31954aac52c449440fc0d1f963721f979
Jennifer Luo <jyl@uoregon.edu>
2020-05-18 18:40:09 -0500

    update fixed page elements and revise menu design

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,26 +5,29 @@ const menu = () => (
     <div className="menu">
         <ul className="menu-container">
             <li className="menu-item">
-                <Link to="/schedule/"><span className="menu-item--label">Schedule</span>
+                <Link to="/schedule/">
                 <span className="menu-item--icon">
-                    <img src="https://placehold.it/20x20" className="menu-item--svg"/>
+                    <img src="https://placehold.it/40x30" className="menu-item--svg"/>
                 </span>
+                    <span className="menu-item--label">Schedule</span>
                 </Link>
             </li>
 
             <li className="menu-item">
-                <Link to="/about/"><span className="menu-item--label">About</span>
+                <Link to="/about/">
                 <span className="menu-item--icon">
-                    <img src="https://placehold.it/20x20" className="menu-item--svg"/>
+                    <img src="https://placehold.it/40x30" className="menu-item--svg"/>
                 </span>
+                    <span className="menu-item--label">About</span>
                 </Link>
             </li>
 
             <li className="menu-item">
-                <Link to="/contact/"><span className="menu-item--label">Contact</span>
+                <Link to="/contact/">
                 <span className="menu-item--icon">
-                    <img src="https://placehold.it/20x20" className="menu-item--svg"/>
+                    <img src="https://placehold.it/40x30" className="menu-item--svg"/>
                 </span>
+                    <span className="menu-item--label">Contact</span>
                 </Link>
             </li>
         </ul>
